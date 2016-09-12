31a78e0bf9eabcf58c00ca7b86f82f083532f3af
JulioGT <julio.gteran@gmail.com>
2016-09-12 10:56:46 +0100

    Total number of students and comments

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# First we print the list with the name of the students from Villains Academy
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,3 +12,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+# Finally, we print the total number of students
+print "Overall, we have"
+# It's important that print() doesn't add a new character at the end of the sentence
+print 11
+puts "great students"
