38dd3116908d5e96f28831b4c3f9862c12ca22b3
JulioGT <julio.gteran@gmail.com>
2016-09-14 14:14:21 +0100

    Refactored code

--- interactive_menu.rb
+++ interactive_menu.rb
@@ -1,44 +1,36 @@
+@students = []
+
 def interactive_menu
-  # Declare students variable before the loop setting it to an empty array
-  # In order to be available in several iterations of the loop, not just in the 1st iteration.
-  students = []
-  # 4. repeat from step 1
   loop do
-    # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    # 2. read the input and save it into a variable
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def process(selection)
+  case selection
     when "1"
       # input the students
-      students = input_students
+      input_students
     when "2"
       # show the students
-      print_header(students)
-      print_indexed(students)
-      print_footer(students)
+      show_students
     when "9"
       exit # this will cause the program to terminate
     else
       puts "I don't know what you meant, try again"
-    end
-  end
-end
-
-def print_header(names)
-  if names.size > 0
-    puts "The students of Villains Academy".center(50)
-    puts "-------------".center(50)
   end
 end
 
 def input_students
   puts "Please enter the names and cohort of the students"
   puts "To finish, just hit return twice"
-  students = []
   puts "Enter the name"
   name = gets.chomp
   puts "Now enter the cohort"
@@ -65,11 +57,11 @@ def input_students
     correct = gets.chomp
   end
   while !name.empty? do
-    students << {name: name, cohort: cohort}
-    if students.size == 1
-      puts "Now we have #{students.count} student:"
+    @students << {name: name, cohort: cohort}
+    if @students.size == 1
+      puts "Now we have #{@students.count} student:"
     else
-      puts "Now we have #{students.count} students:"
+      puts "Now we have #{@students.count} students:"
     end
     puts "#{name} has been added to #{cohort} cohort."
     puts "Enter a new name"
@@ -100,15 +92,29 @@ def input_students
       puts "Right #{name} you have been assigned to #{cohort} cohort"
     end
   end
-  students.each do |name|
+  @students.each do |name|
     name[:hobby] = "coding"
     name[:country_birth] = "Spain"
     name[:food] = "paella"
   end
 end
 
+def show_students
+  print_header(@students)
+  print_students_list(@students)
+  print_footer(@students)
+end
 
-def print_indexed(names)
+
+def print_header(names)
+  if names.size > 0
+    puts "The students of Villains Academy".center(50)
+    puts "-------------".center(50)
+  end
+end
+
+
+def print_students_list(names)
   if names.size > 0
     names.each.with_index(1) do |name, i|
       print "#{i}. #{name[:name]} (#{name[:cohort]} cohort) is from #{name[:country_birth]}"
